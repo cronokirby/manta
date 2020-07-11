f5435b11f82d68ad2db32482c7314c471e323ae5
Lúcás Meier <cronokirby@gmail.com>
2020-07-11 16:25:06 +0200

    Add basic image type
    This creates an image type to allow us to set pixels, and to abstract
    over image writing. Right now I have it set to output ppm images, but we
    can easily add support for png by pulling in a crate.

---
+++ src/image.rs
@@ -0,0 +1,78 @@
+use std::io;
+
+/// Represents an RGBA color / pixel
+///
+/// This is our main representation of colors, and a pretty simple struct as well
+#[derive(Clone, Copy, Debug)]
+pub struct RGBA {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+/// Create a new RGBA color with full opacity
+///
+/// This is a bit shorter than the `rgba` function in the common case where you
+/// don't want any transparency.
+pub fn rgb(r: u8, g: u8, b: u8) -> RGBA {
+    RGBA { r, g, b, a: 255 }
+}
+
+/// Represents an Image.
+///
+/// In our conception, an image is simply a 2D collection of RGBA pixels.
+/// Images are the canvas on which we draw data, and are the final step before we
+/// output them to a file
+#[derive(Clone, Debug)]
+pub struct Image {
+    // We could have an array of pixels instead, but seperating out the components is
+    // usually a bit friendlier towards writing to a file. Namely, the PNG crate we use
+    // expects a contiguous array of bytes
+    data: Vec<u8>,
+    // The width of the image
+    width: usize,
+    // The height of the image
+    height: usize,
+}
+
+impl Image {
+    /// Create an empty image.
+    ///
+    /// Visually, this will be filled with transparent black pixels
+    pub fn empty(width: usize, height: usize) -> Self {
+        let data = vec![0; (width * height) << 2];
+        Image {
+            data,
+            width,
+            height,
+        }
+    }
+
+    /// Set a single pixel in the image
+    ///
+    /// The coordinate system varies in the standard way, with (x: 0, y: 0) being
+    /// the top left corner, and (width - 1, height - 1) being the bottom right corner.
+    ///
+    /// Trying to set an out of bounds pixel will panic, but not with the nicest error
+    /// message.
+    pub fn set(&mut self, x: usize, y: usize, color: RGBA) {
+        let start = (self.width * y + x) << 2;
+        self.data[start] = color.r;
+        self.data[start + 1] = color.g;
+        self.data[start + 2] = color.b;
+        self.data[start + 3] = color.a;
+    }
+
+    pub fn write_ppm<W: io::Write>(&mut self, mut w: W) -> io::Result<()> {
+        writeln!(&mut w, "P6")?;
+        writeln!(&mut w, "{} {} 255", self.width, self.height)?;
+        let mut no_alpha = Vec::with_capacity(self.width * self.height * 3);
+        for i in 0..self.data.len() {
+            if ((i + 1) & 0b11) != 0 {
+                no_alpha.push(self.data[i]);
+            }
+        }
+        w.write_all(&no_alpha)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod image;
+use image::{rgb, Image};
+use std::fs::File;
+use std::io;
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+    let mut img = Image::empty(3, 2);
+    img.set(0, 0, rgb(255, 0, 0));
+    img.set(1, 0, rgb(0, 255, 0));
+    img.set(2, 0, rgb(0, 0, 255));
+    img.set(0, 1, rgb(0, 0, 0));
+    img.set(1, 1, rgb(100, 100, 100));
+    img.set(2, 1, rgb(255, 255, 255));
+    let path = Path::new("out/image.ppm");
+    let file = File::create(path)?;
+    let mut w = io::BufWriter::new(file);
+    img.write_ppm(&mut w)
 }
