67c2da7f9a148be3fb1d007610eb59ae0d15fe0f
Lúcás Meier <cronokirby@gmail.com>
2020-07-15 19:36:57 +0200

    Remove existing stuff
    It's easier, as we move on to physically based rendering, to remove the
    ray tracing code we have now, and start from scratch more or less.
    
    "You should be using a new repo" you despair! Sure. But I don't feel
    like it. "Manta" is a pretty good name for a ray tracer, and my intent
    was always to make it useable as a standalone program, taking input from
    files.
    
    Since I've yet to achieve the goals I've envisioned, and want to have a
    more grounded approach in theory, I think it's best to reuse the same
    thing.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,3 @@
-mod geometry;
-mod image;
-mod tracer;
-
-use std::fs::File;
-use std::io;
-use std::path::Path;
-#[cfg(feature = "png")]
-extern crate png;
-extern crate fastrand;
-
-fn main() -> io::Result<()> {
-    let width = 1000;
-    let height: usize = width / 16 * 9;
-
-    let img = tracer::trace(width, height);
-
-    #[cfg(feature = "png")]
-    {
-        let path = Path::new("out/image.png");
-        let file = File::create(path)?;
-        let mut w = io::BufWriter::new(file);
-        if let Err(e) = img.write_png(&mut w) {
-            eprintln!("Failed to write image: {}", e);
-        }
-    }
-    #[cfg(not(feature = "png"))]
-    {
-        let path = Path::new("out/image.ppm");
-        let file = File::create(path)?;
-        let mut w = io::BufWriter::new(file);
-        img.write_ppm(&mut w)?;
-    }
-    Ok(())
+fn main() {
+    println!("Hello World!");
 }
